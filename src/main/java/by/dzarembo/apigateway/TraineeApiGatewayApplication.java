c37b2fdc9d19bf371fcc7292ac9f5e900f574ccd
dzarembo <dima.zarembo@gmail.com>
2026-04-06 16:49:08 +0300

    init config

---
+++ src/main/java/by/dzarembo/apigateway/TraineeApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package by.dzarembo.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraineeApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/dzarembo/apigateway/TraineeApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dzarembo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
